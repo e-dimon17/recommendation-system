c9664a80a0b36c8ce2fc6d2e5f9a2bf79a31ebf0
echostorm17 <skydev359@outlook.com>
2024-09-06 00:53:41 -0400

    feat: run server

--- run_model.py
+++ run_model.py
@@ -12,10 +12,21 @@ model = joblib.load('svd_book_recommendation_model.pkl')
 class RatingRequest(BaseModel):
     user_id: str
     item_id: str
-    
+
 # Define the prediction route
 @app.post('/predict')
 def predict_rating(data: RatingRequest):
     # Make prediction
     prediction = model.predict(data.user_id, data.item_id).est
     return {'predicted_rating': prediction}
+
+import uvicorn
+import nest_asyncio
+from pyngrok import ngrok
+
+nest_asyncio.apply()
+
+public_url = ngrok.connect(9003, "http")
+print('Public URL:', public_url)
+
+uvicorn.run(app, host='0.0.0.0', port=9003)
