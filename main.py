80a51782b34792d1e5d9f64fd856380da7db8412
echostorm17 <skydev359@outlook.com>
2024-09-06 00:48:06 -0400

    feat: add recommendation

--- main.py
+++ main.py
@@ -16,3 +16,10 @@ ratings = pd.read_csv(file_name)
 
 # Display the first few rows
 ratings.head()
+
+from surprise import Dataset, Reader, SVD
+from surprise.model_selection import cross_validate
+
+# Load data into Scikit-surprise format
+reader = Reader(rating_scale=(1, 10))
+data = Dataset.load_from_df(ratings[['User-ID', 'ISBN', 'Book-Rating']], reader)
